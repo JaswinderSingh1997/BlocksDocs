5a7da60f01cbad4abd74cbd249c7ce8e91bdf9e7
Jaswinder Singh <jaswinder.singh03@squareyards.co.in>
2024-01-16 15:20:41 +0530

    removed unnessary files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,6 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'Blocks Documentation',
         logo: {
